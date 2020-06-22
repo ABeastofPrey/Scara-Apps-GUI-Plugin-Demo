022ae9a06d866c96dec5f84278a54bef6b6bb301
Wang1 <Wenbo.Wang1@kuka.com>
2020-06-22 16:32:51 +0800

    customize react web component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import './App.css';
 import Profile from './profile/Profile';
+import ReactWebComponent from 'react-web-component';
 
-import ReactDOM from 'react-dom';
-
-class XSearch extends HTMLElement {
-  connectedCallback() {
-    const mountPoint = document.createElement('span');
-    this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
-
-    const name = this.getAttribute('name') as string;
-    const url = 'https://www.google.com/search?q=' + encodeURIComponent(name);
-    ReactDOM.render(<a href={url}>{name}</a>, mountPoint);
+function HelloWorld() {
+  return (
+    <div>
+      Hello World!!!
+    </div>
+  )
+}
+ 
+class Hello extends React.Component {
+  render() {
+    return <HelloWorld />
   }
 }
 
-
+ReactWebComponent.create(<Hello />, 'user-profile');
+// ReactWebComponent.create(<HelloWorld />, 'user-profile');
 
 function App() {
-  customElements.define('x-search', XSearch);
   return (
     <div className="App">
       <header className="App-header">
         <Profile />
-        <x-search></x-search>
+        <user-profile></user-profile>
       </header>
     </div>
   );
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,5 +1,5 @@
 declare namespace JSX {
     interface IntrinsicElements {
-        "x-search": any;
+        'user-profile': any;
     }
 }
